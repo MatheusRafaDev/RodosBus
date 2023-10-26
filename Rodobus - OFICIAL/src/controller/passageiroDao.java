e07e4414d6f822c57a081eb1de5e8299f495cebb
TH_Dev <thiago.silva10051.ts@gmail.com>
2023-10-26 17:14:13 -0300

    fazendo table Passageiro

--- Rodobus - OFICIAL/src/controller/passageiroDao.java
+++ Rodobus - OFICIAL/src/controller/passageiroDao.java
@@ -1,12 +1,10 @@
 package controller;
 
-import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import javax.swing.JOptionPane;
-import models.Passageiro;
+import model.Passageiro;
 
 public class passageiroDao extends conectarDao {
     private PreparedStatement ps;
@@ -14,21 +12,23 @@ public class passageiroDao extends conectarDao {
         super();
     }
 
-    public void incluir(Passageiro obj) {
-          sql = "INSERT INTO USUARIOS VALUES (?, ?, ?, ?, ?, ?)";
+    public void cadastrar(Passageiro obj) {
+          sql = "INSERT INTO TB_PASSAGEIRO (DS_CPF, DS_NOME, DS_EMAIL, DS_TELEFONE, NR_IDADE, DS_SENHA)(?, ?, ?, ?, ?, ?)";
         try {
-            ps = con.prepareStatement(sql);
+            ps = mycon.prepareStatement(sql);
+            
             ps.setString(1, obj.getCpf());
             ps.setString(2, obj.getNome());
             ps.setString(3, obj.getEmail());
             ps.setString(4, obj.getTelefone());
             ps.setInt(5, obj.getIdade());
             ps.setString ( 6, obj.getSenha());
+            
             ps.execute();
             ps.close();
             JOptionPane.showMessageDialog(null,"Cadastro concluído com Sucesso!");
         } catch (SQLException err) {
-            JOptionPane.showMessageDialog(null, "Erro ao incluir usuário!" + err.getMessage());
+            JOptionPane.showMessageDialog(null, "Erro ao Cadastrar!" + err.getMessage());
         }
     }
     
@@ -39,10 +39,12 @@ public class passageiroDao extends conectarDao {
             + "and senha = ucase('"+ senha +"')";
    
     try {
-            ps = con.prepareStatement(sql);
+            ps = mycon.prepareStatement(sql);
             return ps.executeQuery();
         } catch (SQLException err) {
             JOptionPane.showMessageDialog(null, "Erro ao Buscar usuário!" + err.getMessage());
+            JOptionPane.showMessageDialog(null, "Login ou Senha Inválidos", "Erro de operação", JOptionPane.WARNING_MESSAGE);
+            
             return null; 
         }
 }
