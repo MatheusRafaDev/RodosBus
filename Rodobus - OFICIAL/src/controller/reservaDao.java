601f65b933a61bab8753e1d304f6d1891c7d45a2
TH_Dev <thiago.silva10051.ts@gmail.com>
2023-11-15 22:29:21 -0300

    Update reservaDao.java
    Adicionando CRUD na reserva

--- Rodobus - OFICIAL/src/controller/reservaDao.java
+++ Rodobus - OFICIAL/src/controller/reservaDao.java
@@ -4,10 +4,38 @@
  */
 package controller;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import model.Reserva;
+
 /**
  *
  * @author thiag
  */
 public class reservaDao {
     
+    private PreparedStatement ps;
+    
+    public reservaDao() {
+        super();
+    }
+    
+    public void incluir(Reserva obj) {
+        sql = "INSERT INTO TB_MOTORISTA (DS_NOME, NR_IDADE, DS_CPF, DS_TELEFONE) VALUES (?, ?, ?, ?)";
+        try {
+            ps = mycon.prepareStatement(sql);
+
+            ps.setString(1, obj.getNome());
+            ps.setInt(2, obj.getIdade());
+            ps.setString(3, obj.getCpf());
+            ps.setString(4, obj.getTelefone());
+
+            ps.execute();
+            ps.close();
+            JOptionPane.showMessageDialog(null,"Registro Cadastrado com Sucesso !");
+        } catch (SQLException err) {
+            
+        }
+    }
 }
